514ed9c051dcde160f95676151de95cf287fc94e
Eduardo Salazar <56232104+eduardosalaz@users.noreply.github.com>
2019-12-15 18:05:38 -0600

    Update temperaturas_rs.rs

--- temperaturas_rs.rs
+++ temperaturas_rs.rs
@@ -3,6 +3,7 @@ fn main() {
 
     let mut escala_inicial = String::new();
     let mut escala_final = String::new();
+    let mut temp_inicial = String::new();
     let mut temp_final = String::new();
 
     println!("Programa de convertidor de temperaturas.");
@@ -13,26 +14,22 @@ fn main() {
         .expect("Lo siento, no entendí eso muy bien");
     escala_inicial = escala_inicial.to_lowercase().trim().to_string();
     
-    if escala_inicial == "celsius"{
-        let mut temp_inicial = String::new();
+    if escala_inicial == "celsius" {
         imprimir_inicial(escala_inicial);
         io::stdin().read_line(&mut temp_inicial);
-        let inicial: u32 = temp_inicial.trim().parse();
-    } else if escala_inicial == "fahrenheit"{
-        let mut temp_inicial = String::new();
+        let inicial: f64 = temp_inicial.trim().parse();
+    } else if escala_inicial == "fahrenheit" {
         imprimir_inicial(escala_inicial);
-    } else if escala_inicial == "kelvin"{
-        let mut temp_inicial = String::new();
+    } else if escala_inicial == "kelvin" {
         imprimir_inicial(escala_inicial);
-    } else if escala_inicial == "rankine"{
-        let mut temp_inicial = String::new();
+    } else if escala_inicial == "rankine" {
         imprimir_inicial(escala_inicial);
     } else {
         println!("Por favor ingresa una escala válida")
     }
 }
 
-fn imprimir_inicial(escala_inicial: String){
+fn imprimir_inicial(escala_inicial: String) {
     println!("Escogiste la escala {}", escala_inicial);
     println!("Ingresar la temperatura inicial en número (dígito)")
 }
