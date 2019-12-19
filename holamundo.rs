343613058ca9bdf06933d28086ca9d275d2ede03
Eduardo Salazar <56232104+eduardosalaz@users.noreply.github.com>
2019-12-19 13:40:52 -0600

    Update holamundo.rs
    eliminar codigo bloat

--- holamundo.rs
+++ holamundo.rs
@@ -1,9 +1,3 @@
 fn main() {
   println!("Hola mundo!");
 }
-
-fn parsear_temp_inicial(mut temp_inicial: String) -> f32{
-  temp_inicial = temp_inicial.trim().to_string();
-  let inicial: f32 = temp_inicial.parse::<f32>().unwrap();
-  return inicial;
-}
