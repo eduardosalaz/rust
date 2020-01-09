7f98651d158fc3ad3464aa3a94a851cdbb184f5a
eduardosalaz <eduardosalaz@outlook.com>
2020-01-08 19:38:31 -0600

    borre cosas de compilado

--- juego_adivinanza/target/debug/deps/juego_adivinanza.d
+++
@@ -1,5 +0,0 @@
-C:\Users\eduar\Documents\GitHub\rust\juego_adivinanza\target\debug\deps\juego_adivinanza.exe: src\main.rs
-
-C:\Users\eduar\Documents\GitHub\rust\juego_adivinanza\target\debug\deps\juego_adivinanza.d: src\main.rs
-
-src\main.rs:
--- juego_adivinanza/target/debug/juego_adivinanza.d
+++
@@ -1,1 +0,0 @@
-C:\Users\eduar\Documents\GitHub\rust\juego_adivinanza\target\debug\juego_adivinanza.exe: C:\Users\eduar\Documents\GitHub\rust\juego_adivinanza\src\main.rs
