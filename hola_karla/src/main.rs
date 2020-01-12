2f31f9e086651d0b58eec8770025c68d0f026290
eduardosalaz <eduardosalaz@outlook.com>
2020-01-11 18:02:54 -0600

    hellow karla 7 times

---
+++ hola_karla/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut contador:u32 = 1;
+    while contador <8 {
+    	println!("Hola karlita {} veces", contador);
+        contador += 1;
+        }   
+}
---
+++ hola_karla/target/debug/deps/hola_karla-98b7afe5b787f435.d
@@ -0,0 +1,5 @@
+/data/data/com.termux/files/home/storage/rust/hola_karla/target/debug/deps/hola_karla-98b7afe5b787f435: src/main.rs
+
+/data/data/com.termux/files/home/storage/rust/hola_karla/target/debug/deps/hola_karla-98b7afe5b787f435.d: src/main.rs
+
+src/main.rs:
